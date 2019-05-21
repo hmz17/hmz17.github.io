3aedf9b36cc085521bb097df8ed920c127d2f99a
Hamza Mirza <hamzamirza95@gmail.com>
2019-05-21 14:06:22 -0400

    Adding information

--- sendmail.php
+++ sendmail.php
@@ -1,10 +1,14 @@
-<?php
-$to      = 'hamzamirza95@gmail.com';
-$subject = 'the subject';
-$message = 'hello';
-$headers = 'From: webmaster@example.com' . "\r\n" .
-    'Reply-To: webmaster@example.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
 
-mail($to, $subject, $message, $headers);
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "hamza.mirza@nyu.edu";
+$subject = "Contact Form from website";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You for contacting!";
+die()
+
 ?>
